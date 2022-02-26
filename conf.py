cecfeb90e40dcd3e979e46646a82e0589fc1b3e2
Wong Sai Ho <sawong@sg.pepperl-fuchs.com>
2022-02-26 19:21:00 +0800

    hoverxref

--- conf.py
+++ conf.py
@@ -31,6 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'hoverxref.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,6 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+hoverxref_auto_ref = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
