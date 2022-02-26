02cf9abafd5351eac506ed240b4014751239d438
Wong Sai Ho <sawong@sg.pepperl-fuchs.com>
2022-02-26 19:45:46 +0800

    hoverxref

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-hoverxref_auto_ref = False
+hoverxref_auto_ref = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
